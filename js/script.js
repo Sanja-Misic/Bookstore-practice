03cf427c748251a02967370d26f86b86f95264be
Sanja-Misic <sanjamisic98@gmail.com>
2023-02-21 11:52:52 +0100

    new commit

--- js/script.js
+++ js/script.js
@@ -1,34 +1,34 @@
 'use strict';
+
+const searchButton = document.querySelector('.header-search-btn')
+const homeNavigation = document.querySelector('.navigation-home');
+const booksNavigation = document.querySelector('.navigation-books');
+const checkBox = document.querySelector('#check-box')
+
 const homePage = document.querySelector('.home-container');
 const booksPage = document.querySelector('.books-container');
 const clickedBookPage = document.querySelector('.book-container');
 
 const imputedText = document.querySelector('.header-search-input');
 
-
 const genreContainer = document.querySelector('.categories-list');
 const genreListItems = document.querySelectorAll('.categories-list-item');
+
 const bestRatingContainer = document.querySelector('.best-rating-item-wrapper');
-const mostReviewContainer = document.querySelector(
-  '.most-revievs-item-wrapper'
-);
-const allBooksContainer = document.querySelector('.all-books-item-wrapper');
-
-const homeNavigation = document.querySelector('.navigation-home');
-const booksNavigation = document.querySelector('.navigation-books');
-
+const mostReviewContainer = document.querySelector('.most-revievs-item-wrapper');
 const randomGendreHeading = document.querySelector('.section-heading-gendre');
-
-const searchButton = document.querySelector('.header-search-btn')
+const allBooksContainer = document.querySelector('.all-books-item-wrapper');
 
 const red = "ff0000"
 const green = "008000"
 
 let genreArr = [];
 let uniqueGenreArr = [];
+let clickBooks;
 let averageRating;
-
-
+let ratingElement;
+let backButton;
+let bookDataArray;
 
 //click on book functionality
 ////Get Books
@@ -44,11 +44,31 @@ const getBooks = async function (books) {
     //   },
     // }
   );
-
   const data = await response.json();
+  let bookData = data.results;
 
-  const bookData = data.results;
-  console.log(bookData);
+  //get array all books and array less than 18 years old
+  const allBooksData = data.results
+  console.log(allBooksData);
+
+  //array genrs to exclude
+  const excludedGenres = ["Adult", "Adult Fiction", "Category Romance", "Crime", "Erotic Horror", "Erotic Romance", "Erotica", "Gay Erotica", "Gay For You", "Gay Romance", "Horor", "Lesbian Fiction", "Lesbian Romance"];
+  const filteredBooks = bookData.filter(book => !excludedGenres.some(genre => book.genre.includes(genre)));
+  console.log(filteredBooks);
+
+  //check box
+  const checkBoxFunction = () => {
+    if(checkBox.checked) {
+      console.log("cekirano")
+      bookDataArray = filteredBooks
+      console.log(bookDataArray) 
+    } else {
+      console.log("nije cekirano")
+      bookDataArray = allBooksData
+      console.log(bookDataArray)
+    }
+  }
+  checkBox.addEventListener("click", checkBoxFunction)
 
   //// navigation funtionality
   homeNavigation.addEventListener('click', function () {
@@ -62,12 +82,12 @@ const getBooks = async function (books) {
     booksPage.style.display = 'flex';
     clickedBookPage.style.display = 'none';
      displayBooks(bookData)
+     clickedBookFunction(bookData, allBooksContainer, booksPage);
   });
 
   ////HOME PAGE
   ////sort by rating
   let bookDataSortRating = bookData.slice();
-
   bookDataSortRating.sort(function (a, b) {
     return b.rating - a.rating;
   });
@@ -79,16 +99,11 @@ const getBooks = async function (books) {
   // get random genre
   let randomBook = bookData[Math.floor(Math.random() * bookData.length)];
   let randomGenres = randomBook.genre.split(',');
-  let randomGenre =
-    randomGenres[Math.floor(Math.random() * randomGenres.length)];
-  // console.log(randomGenre)
+  let randomGenre = randomGenres[Math.floor(Math.random() * randomGenres.length)];
 
   //get array of books in random ganre
-  let booksInRandomGenre = bookData.filter(book =>
-    book.genre.includes(randomGenre)
-  );
-  // console.log(booksInRandomGenre)
-
+  let booksInRandomGenre = bookData.filter(book => book.genre.includes(randomGenre));
+ 
   //sort books by rewiews and get 4 book with best review
   booksInRandomGenre.sort(function (a, b) {
     return b.reviews - a.reviews;
@@ -132,16 +147,13 @@ const getBooks = async function (books) {
   const allGenresList = document.querySelector('.categories-list-item-all');
   allGenresList.addEventListener('click', function () {
     displayBooks(bookData);
-    
     clickedBookFunction(bookData, allBooksContainer, booksPage);
   });
 
-  displayBooks(bookData);
   clickedBookFunction(bestRatingBooks, bestRatingContainer, homePage);
   clickedBookFunction(mostReviewBooks, mostReviewContainer, homePage);
   clickedBookFunction(bookData, allBooksContainer, booksPage);
 
-//////////////////////////proba
  //search functionality
  searchButton.addEventListener('click', function() {
     homePage.style.display = "none"
@@ -150,36 +162,37 @@ const getBooks = async function (books) {
      if(imputedText.value) {
         const searchTerm = imputedText.value.toLowerCase()
         const filterBooks = bookData.filter(book => {
-        const bookTitle = book.title.toString().toLowerCase()
-          
+        const bookTitle = book.title.toString().toLowerCase()  
         return bookTitle.includes(searchTerm)      
         })
-        console.log(filterBooks)
-            
+
         if (filterBooks.length > 0) {
             displayBooks(filterBooks)
+            console.log(filterBooks)
         } else {
-            booksPage.innerHTML = `<div class="wrong-input">
-        <p class="wrong-input-text">You have entered a title that does not exist!</p>
-        <p class="wrong-input-text">
-            Try again!</p>
-        </div>`;
+          allBooksContainer.innerHTML = `<div class="wrong-input">
+        <p class="wrong-input-text"> You have entered a title that does not exist!</p>
+        <p class="wrong-input-text"> Try again!</p> </div>`;
         console.log("wrong title")
+        console.log(filterBooks)
     }
+    } else {
+      console.log("no imputed value")
+      allBooksContainer.innerHTML = `<div class="wrong-input">
+      <p class="wrong-input-text"> You have not entered any search text!</p>
+      <p class="wrong-input-text"> Please enter what you want to search!</p> </div>`;
     }
 })
     ////average rating
     //get all rating and calculate average
     const ratings = bookData.map(book => book.rating);
-    averageRating = ratings.reduce((sum, rating) => sum + rating, 0) / ratings.length;
+    averageRating = (ratings.reduce((sum, rating) => sum + rating, 0) / ratings.length).toFixed(2);
     console.log(averageRating)
 };
 
 getBooks();
 
-
-
-/////functions
+/////Functions
 const displayBestRatingBooks = array => {
   array.forEach(function (book) {
     // console.log(book)
@@ -230,20 +243,13 @@ const displayBooks = array => {
 };
 
 const clickedBookFunction = (array, booksContainer, page) => {
-  const clickBooks = booksContainer.querySelectorAll('.item');
-//   const itemRating = clickedBookPage.querySelector('.book-info-span-rating');
+  clickBooks = booksContainer.querySelectorAll('.item');
   
-  
-
   clickBooks.forEach((book, i) =>
     book.addEventListener('click', function () {
       clickedBookPage.innerHTML = '';
-
       clickedBookPage.style.display = 'block';
       page.style.display = 'none';
-    // booksPage.style.display = 'none'
-
-  
 
       const html = `<div class="book-info-wrapper">
             <img src="${array[i].img}" alt="" class="book-info-img">
@@ -253,7 +259,7 @@ const clickedBookFunction = (array, booksContainer, page) => {
                 <p class="book-info-item">Format: <span class="book-info-span">${array[i].bookformat}</span></p>
                 <p class="book-info-item">Genre: <span class="book-info-span">${array[i].genre}</span></p>
                 <p class="book-info-item">Pages: <span class="book-info-span">${array[i].pages}</span></p>
-                <p class="book-info-item">Rating: <span class="book-info-span-rating">${array[i].rating}</span></p>
+                <p class="book-info-item">Rating: <span class="book-info-span book-info-span-rating">${array[i].rating}</span></p>
             </div>
             </div>
             <div class="book-description">
@@ -263,27 +269,21 @@ const clickedBookFunction = (array, booksContainer, page) => {
             <button class="back-button"> Back &larr; </button>`;
 
       clickedBookPage.insertAdjacentHTML('beforeend', html);
-
-
-    //     const ratingElement = document.querySelector('.book-info-span-rating');
-    //     const ratingValue = ratingElement ? ratingElement.textContent : '';
-
-
+     
+      //Color element according to rating
+      ratingElement = document.querySelector('.book-info-span-rating')    
       if (array[i].rating > averageRating){
-        console.log(array[i].rating, "veci")
-        // console.log(itemRating)
-        // itemRating.style.backgroundColor = "green"
-       } else if (array[i].rating < averageRating){
-        console.log(array[i].rating, "manji")
-        // itemRating.style.backgroundColor = "red"
-       }
+        ratingElement.style.backgroundColor = "green"
+       } else if (array[i].rating < averageRating){   
+        ratingElement.style.backgroundColor = "red"
+       } 
 
-    //Back btn functionality
-    const backButton = document.querySelector('.back-button ');
-    backButton.addEventListener('click', function(){
+      //Back btn functionality
+      backButton = document.querySelector('.back-button ');
+      backButton.addEventListener('click', function(){
         clickedBookPage.style.display = 'none';
         page.style.display = 'flex';
-    })
+      })
     })
   );
 };
